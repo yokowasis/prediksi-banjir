890e735b609cbdcbed66153bfdff48993060646c
yokowasis@nitro5 <yokowasis@gmail.com>
2023-06-18 17:45:09 +0800

    prediksi banjir

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<div class="container">
+  <div class="row d-flex justify-content-center align-items-center vh-100">
+    <div class="col col-sm-8">
+      <div class="card">
+        <div class="card-header bg-primary text-white">Login</div>
+        <div class="card-body text-center">
+          <a href="/prediksi"
+            ><img src="/google.png" alt="login" style="width:200px" /></a
+          >
+          <p>--------- or ---------</p>
+          <form class="mt-3">
+            <div class="form-group mb-3">
+              <input
+                type="text"
+                class="form-control"
+                id="username"
+                placeholder="Enter username"
+              />
+            </div>
+            <div class="form-group mb-3">
+              <input
+                type="password"
+                class="form-control"
+                id="password"
+                placeholder="Enter password"
+              />
+            </div>
+            <a href="/prediksi" class="btn btn-primary px-5"> Login </a>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ src/routes/prediksi/+page.svelte
@@ -0,0 +1,99 @@
+<div class="container mt-3">
+  <div class="row">
+    <div class="col">
+      <div class="card">
+        <div class="card-header bg-primary text-white">Prediksi</div>
+        <div class="card-body">
+          <div class="form-group mb-3 text-center">
+            <label for="nama" class="mb-2">Propinsi</label>
+            <input
+              type="text"
+              class="form-control text-center"
+              id="propinsi"
+              value="Jakarta"
+            />
+          </div>
+          <div class="form-group mb-3 text-center">
+            <label for="nama" class="mb-2">Kota / Kab.</label>
+            <select class="form-control text-center">
+              <option value="Jakarta Pusat">Jakarta Pusat</option>
+              <option value="Jakarta Barat">Jakarta Barat</option>
+              <option value="Jakarta Selatan">Jakarta Selatan</option>
+              <option value="Jakarta Timur">Jakarta Timur</option>
+              <option value="Jakarta Utara">Jakarta Utara</option>
+            </select>
+          </div>
+          <div class="form-group mb-3 text-center">
+            <label for="nama" class="mb-2">Nama Pintu Air</label>
+            <select class="form-control text-center">
+              <option value="PA. Cibalok - Gadog">PA. Cibalok - Gadog</option>
+              <option value="P.A. Hek">P.A. Hek</option>
+              <option value="PA. Pulo Gadung">PA. Pulo Gadung</option>
+              <option value="PS. Cipinang Hulu">PS. Cipinang Hulu</option>
+              <option value="PS. Krukut Hulu">PS. Krukut Hulu</option>
+              <option value="PS. Angke Hulu">PS. Angke Hulu</option>
+              <option value="Pompa Yos Sudarso 1">Pompa Yos Sudarso 1</option>
+              <option value="PS. Katulampa (Hulu)">PS. Katulampa (Hulu)</option>
+              <option value="PS. Depok">PS. Depok</option>
+              <option value="PA. Manggarai">PA. Manggarai</option>
+              <option value="Pompa Cideng">Pompa Cideng</option>
+              <option value="P.A. Karet">P.A. Karet</option>
+              <option value="P.A. Marina Ancol ">P.A. Marina Ancol </option>
+              <option value="Pompa Pasar Ikan">Pompa Pasar Ikan</option>
+              <option value="P.A. Pluit">P.A. Pluit</option>
+              <option value="PS. Pesanggrahan">PS. Pesanggrahan</option>
+              <option value="PS. Sunter Hulu">PS. Sunter Hulu</option>
+              <option value="Pompa Kali Duri (Kalijodo)"
+                >Pompa Kali Duri (Kalijodo)</option
+              >
+              <option value="P.A. Istiqlal  ">P.A. Istiqlal </option>
+              <option value="P.A. Jembatan Merah">P.A. Jembatan Merah</option>
+              <option value="P.A. Ancol Flusing">P.A. Ancol Flusing</option>
+              <option value="P.A. Flusing Ancol">P.A. Flusing Ancol</option>
+              <option value="Bendung. Cibalok - Gadog"
+                >Bendung. Cibalok - Gadog</option
+              >
+              <option value="Bendung. Katulampa (Hulu)"
+                >Bendung. Katulampa (Hulu)</option
+              >
+              <option value="Pompa. Pluit">Pompa. Pluit</option>
+            </select>
+          </div>
+          <div class="form-group mb-3 text-center">
+            <label for="nama" class="mb-2">Kota / Kab.</label>
+            <input class="text-center form-control" type="number" />
+          </div>
+          <div class="text-center">
+            <button class="btn btn-primary px-5">Proses</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <table class="table table-bordered mt-3">
+        <thead>
+          <tr>
+            <th scope="col">No</th>
+            <th scope="col">Propinsi</th>
+            <th scope="col">Kota / Kab.</th>
+            <th scope="col">Nama Pintu Air</th>
+            <th scope="col">Tinggi Muka Air</th>
+            <th scope="col">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Jakarta</td>
+            <td>Jakarta Pusat</td>
+            <td>PA. Cibalok - Gadog</td>
+            <td>100</td>
+            <td class="text-danger fw-bold">Siaga 1</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
